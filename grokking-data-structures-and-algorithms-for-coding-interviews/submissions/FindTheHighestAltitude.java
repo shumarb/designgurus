5a0c057cede4bf68cf8bff98b45936f94734ad78
Umar <shumarb@outlook.com>
2025-01-25 14:38:03 +0800

    Updated submission with same runtime and improved space complexity [O(n) -> O(1)]

--- grokking-data-structures-and-algorithms-for-coding-interviews/submissions/FindTheHighestAltitude.java
+++ grokking-data-structures-and-algorithms-for-coding-interviews/submissions/FindTheHighestAltitude.java
@@ -1,20 +1,13 @@
 public class FindTheHighestAltitude {
     public int largestAltitude(int[] gain) {
-        boolean isTest = false;
-
-        int[] prefixSumArr = new int[gain.length];
-        prefixSumArr[0] = gain[0];
-        for (int i = 1; i < gain.length; i++) {
-            prefixSumArr[i] = gain[i] + prefixSumArr[i - 1];
+        public int largestAltitude(int[] gain) {
+            int maxAltitude = 0;
+            int cumulativeAltitude = 0;
+            for (int i = 0; i < gain.length; i++) {
+                cumulativeAltitude += gain[i];
+                maxAltitude = Math.max(maxAltitude, cumulativeAltitude);
+            }
+            return maxAltitude;
         }
-        if (isTest) {
-            System.out.println("gain: " + Arrays.toString(gain));
-            System.out.println("prefixSumArr: " + Arrays.toString(prefixSumArr));
-        }
-        int maxAltitude = prefixSumArr[0];
-        for (int prefixSum: prefixSumArr) {
-            maxAltitude = Math.max(maxAltitude, prefixSum);
-        }
-        return maxAltitude < 0 ? 0 : maxAltitude;
     }
 }
