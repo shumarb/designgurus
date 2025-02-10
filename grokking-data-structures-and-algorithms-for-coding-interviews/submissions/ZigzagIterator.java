9f4118a960bf43297d1f17c8ed33c609d0b35668
Umar <shumarb@outlook.com>
2025-02-11 01:28:10 +0800

    Zigzag Iterator: Updated solution with reduced code and use of 1 queue

--- grokking-data-structures-and-algorithms-for-coding-interviews/submissions/ZigzagIterator.java
+++ grokking-data-structures-and-algorithms-for-coding-interviews/submissions/ZigzagIterator.java
@@ -3,33 +3,50 @@
 import java.util.*;
 
 class ZigzagIterator {
-    private Queue<Integer> queue1;
-    private Queue<Integer> queue2;
+    private Queue<Integer> queue;
 
-    public Solution(List<Integer> v1, List<Integer> v2) {
-        queue1 = new LinkedList<>();
-        queue2 = new LinkedList<>();
-        populate(v1, queue1);
-        populate(v2, queue2);
+    public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+        queue = new LinkedList<>();
+        populate(queue, v1, v2);
     }
 
-    private void populate(List<Integer> v1, Queue<Integer> queue) {
-        for (Integer number: v1) {
-            queue.offer(number);
+    private void populate(Queue<Integer> queue, List<Integer> v1, List<Integer> v2) {
+        int current = 1;
+        int i = 0;
+        int j = 0;
+        boolean isTest = false;
+
+        if (isTest) {
+            System.out.println("v1: " + v1 + ", v2: " + v2);
+        }
+
+        while (i < v1.size() || j < v2.size()) {
+            if (current == 1) {
+                if (i < v1.size()) {
+                    queue.offer(v1.get(i++));
+                }
+                current = 2;
+            } else {
+                if (j < v2.size()) {
+                    queue.offer(v2.get(j++));
+                }
+                current = 1;
+            }
+            if (isTest) {
+                System.out.println("queue so far: " + queue);
+            }
+        }
+
+        if (isTest) {
+            System.out.println("updated queue: " + queue);
         }
     }
 
     public int next() {
-        if (!queue1.isEmpty()) {
-            return queue1.poll();
-        }
-        if (!queue2.isEmpty()) {
-            return queue2.poll();
-        }
-        return -1;
+        return queue.isEmpty() ? -1 : queue.poll();
     }
 
     public boolean hasNext() {
-        return (!queue1.isEmpty() || !queue2.isEmpty());
+        return !queue.isEmpty();
     }
 }
